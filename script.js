8cf7f7f4505498b6213b63dbd0b64484366c5f0c
st25511-hub <st25511@ormiston.school.nz>
2026-02-23 22:54:29 +0000

    Protoype hobby

---
+++ script.js
@@ -0,0 +1,83 @@
+const content = [
+{
+title: "Imagine Me & You",
+genre: "romance",
+rating: "8.5/10",
+poster: "https://m.media-amazon.com/images/M/MV5BMjE1NjM4ODU5NF5BMl5BanBnXkFtZTcwNjQ0OTczMQ@@._V1_.jpg",
+trailer: "https://www.youtube.com/watch?v=RzJz3lKX8bA"
+},
+{
+title: "Avengers: Endgame",
+genre: "action",
+rating: "9/10",
+poster: "https://m.media-amazon.com/images/I/81ExhpBEbHL._AC_SL1500_.jpg",
+trailer: "https://www.youtube.com/watch?v=TcMBFSGVi1c"
+},
+{
+title: "Encanto",
+genre: "fantasy",
+rating: "8.7/10",
+poster: "https://m.media-amazon.com/images/I/81O1+YqzPXL._AC_SL1500_.jpg",
+trailer: "https://www.youtube.com/watch?v=CaimKeDcudo"
+},
+{
+title: "The Conjuring",
+genre: "horror",
+rating: "8.3/10",
+poster: "https://m.media-amazon.com/images/I/91Jj+1dXbLL._AC_SL1500_.jpg",
+trailer: "https://www.youtube.com/watch?v=k10ETZ41q5o"
+},
+{
+title: "Stranger Things",
+genre: "fantasy",
+rating: "8.8/10",
+poster: "https://m.media-amazon.com/images/I/81WcnNQ-TBL._AC_SL1500_.jpg",
+trailer: "https://www.youtube.com/watch?v=b9EkMc79ZSU"
+},
+{
+title: "Wednesday",
+genre: "fantasy",
+rating: "8.2/10",
+poster: "https://m.media-amazon.com/images/I/81wKz8XwWbL._AC_SL1500_.jpg",
+trailer: "https://www.youtube.com/watch?v=Di310WS8zLk"
+}
+];
+
+const contentList = document.getElementById("contentList");
+const recommendList = document.getElementById("recommendList");
+
+function displayContent(items) {
+contentList.innerHTML = "";
+
+items.forEach(item => {
+const card = document.createElement("div");
+card.classList.add("card");
+
+card.innerHTML = `
+<img src="${item.poster}">
+<h3>${item.title}</h3>
+<p>⭐ Rating: ${item.rating}</p>
+<button onclick="window.open('${item.trailer}')">Watch Trailer</button>
+<button onclick="addRecommendation('${item.title}')">Recommend</button>
+`;
+
+contentList.appendChild(card);
+});
+}
+
+function filterContent(genre) {
+if (genre === "all") {
+displayContent(content);
+} else {
+const filtered = content.filter(item => item.genre === genre);
+displayContent(filtered);
+}
+}
+
+function addRecommendation(title) {
+const li = document.createElement("li");
+li.textContent = title;
+recommendList.appendChild(li);
+}
+
+displayContent(content);
